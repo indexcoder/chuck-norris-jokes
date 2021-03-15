19afb3917ba081fa7ba851cceb3528bcf81093b2
nurs <programmer.kg@mail.ru>
2021-03-15 12:58:31 +0300

    Initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,24 @@
+<?php
+namespace  Indexcoder\ChuckNorrisJokes;
+
+class JokeFactory {
+
+    protected $jokes = [
+        'Chuck Norris\' tears cure cancer. Too bad he has never cried',
+        'Chuck Norris counted to infinity... Twice.',
+        'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.'
+    ];
+
+    public function __construct(array $jokes = null) {
+        if ( $jokes ) {
+            $this->jokes = $jokes;
+        }
+
+    }
+
+    public function getRandomJoke() {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+
+}
+
