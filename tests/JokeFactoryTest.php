e53c1005185ef47b8f8cd0cd725c6874ec7c1e97
Nurseyit <indexcoder@users.noreply.github.com>
2021-03-16 07:24:12 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -6,24 +6,23 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
-use PHPUnit\Framework\TestCase;
 use Indexcoder\ChuckNorrisJokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
 
 class JokeFactoryTest extends TestCase
 {
     /** @test */
-    public function it_returns_a_random_joke() {
+    public function it_returns_a_random_joke()
+    {
 
         // Create a mock and queue two responses.
         $mock = new MockHandler([
-            new Response(200, [], '{ "type": "success", "value": { "id": 183, "joke": "Chuck Norris drinks napalm to quell his heartburn.", "categories": [] } }')
+            new Response(200, [], '{ "type": "success", "value": { "id": 183, "joke": "Chuck Norris drinks napalm to quell his heartburn.", "categories": [] } }'),
         ]);
 
         $handlerStack = HandlerStack::create($mock);
 
         $client = new Client(['handler' => $handlerStack]);
-
-
 
         $jokes = new JokeFactory($client);
 
@@ -31,6 +30,4 @@ class JokeFactoryTest extends TestCase
 
         $this->assertSame('Chuck Norris drinks napalm to quell his heartburn.', $joke);
     }
-
-
 }
