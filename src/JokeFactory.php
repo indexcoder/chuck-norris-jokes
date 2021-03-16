460fac7c3aff3ee609ce7b08e8e628c1d5d392ce
nurs <programmer.kg@mail.ru>
2021-03-16 06:40:31 +0300

    Add Travis settings

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -1,10 +1,10 @@
 <?php
 namespace  Indexcoder\ChuckNorrisJokes;
-
+// 1.0.1
 class JokeFactory {
 
     protected $jokes = [
-        'Chuck Norris\' tears cure cancer. Too bad he has never cried',
+        'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
         'Chuck Norris counted to infinity... Twice.',
         'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.'
     ];
@@ -19,6 +19,7 @@ class JokeFactory {
     public function getRandomJoke() {
         return $this->jokes[array_rand($this->jokes)];
     }
+
 
 }
 
