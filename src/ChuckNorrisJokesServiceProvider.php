8d94588a5bacf8133c3b57e7cef7ae31312e80e0
nurs <programmer.kg@mail.ru>
2021-03-17 05:55:51 +0300

    console comand add

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -3,11 +3,17 @@
 namespace  Indexcoder\ChuckNorrisJokes;
 
 use Illuminate\Support\ServiceProvider;
+use Indexcoder\ChuckNorrisJokes\Console\ChuckNorrisJoke;
 
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if (app()->runningInConsole()) {
+            $this->commands([
+                ChuckNorrisJoke::class
+            ]);
+        }
     }
 
     public function register()
